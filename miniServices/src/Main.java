f9287e78e9061563ed390a56ece0488bfc68f5a3
Bahaa Abu-alrub <babualrub@babualrub.atypon.com>
2023-10-21 12:43:40 +0300

    cleanup

--- miniServices/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        CommandRunner.executeCommand("netsh advfirewall firewall add rule name=\"Block All Internet Access\" dir=out action=block enable=yes");
-        CommandRunner.executeCommand("netsh advfirewall firewall delete rule name=\"Block All Internet Access\"");
-        CommandRunner.executeCommand("start chrome \"https://www.youtube.com/\"");
-        CommandRunner.executeCommand("start C:\\Users\\Saif\\AppData\\Local\\Microsoft\\Teams\\Update.exe --processStart \"Teams.exe\"");
-    }
-}
