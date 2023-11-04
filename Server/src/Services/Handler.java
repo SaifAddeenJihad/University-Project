b6adc5b30a936310b2ff92bef0058f1a137ab6d3
saif <106633076+SaifAddeenJihad@users.noreply.github.com>
2023-11-04 17:31:31 +0300

    0

--- Server/src/Services/Handler.java
+++ Server/src/Services/Handler.java
@@ -3,35 +3,39 @@ package Services;
 import Stream.Capture;
 import Stream.Sender;
 import auxiliaryClasses.Client;
+import javafx.collections.ObservableList;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 public class Handler {
     private Handler(){}
     public static volatile Queue<byte[]> baos =new LinkedList<>();
-    public static void startStream(Map<Integer, Client> data) throws UnknownHostException {
+    public static void startStream(List<String> IPs) throws UnknownHostException {
 
-        ArrayList<InetAddress> ipAdresses = getIneAdresses((ArrayList<Client>) data.values());
+        ArrayList<InetAddress> ipAdresses = getIneAdresses((ObservableList<String>) IPs);
         CommandService.sendCommand(ipAdresses, Commands.STREAM);
         Thread t1=new Thread(new Capture());
         t1.start();
         Thread t2=new Thread(new Sender());
         t2.start();
     }
-    public static void closeStream(Map<Integer, Client> data) throws UnknownHostException {
-        ArrayList<InetAddress> ipAdresses = getIneAdresses((ArrayList<Client>) data.values());
+    public static void closeStream(List<String> IPs) throws UnknownHostException {
+        ArrayList<InetAddress> ipAdresses = getIneAdresses((ObservableList<String>) IPs);
         CommandService.sendCommand(ipAdresses, Commands.CLOSE_STREAM);
     }
 
-    private static ArrayList<InetAddress> getIneAdresses(ArrayList<Client> clients) throws UnknownHostException {
+    /*private static ArrayList<InetAddress> getIneAdresses(ArrayList<Client> clients) throws UnknownHostException {
         ArrayList<InetAddress> ipAdresses =new ArrayList<>();
         for (Client client: clients)
             ipAdresses.add(InetAddress.getByName(client.getIpAddress()));
+        return ipAdresses;
+    }*/
+    private static ArrayList<InetAddress> getIneAdresses(ObservableList<String> IPs) throws UnknownHostException {
+        ArrayList<InetAddress> ipAdresses =new ArrayList<>();
+        for (String IP: IPs)
+            ipAdresses.add(InetAddress.getByName(IP));
         return ipAdresses;
     }
 }
